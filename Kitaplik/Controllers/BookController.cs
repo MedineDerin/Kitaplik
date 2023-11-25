21269c80612980fa3ba570b02ce8a7967e787d46
Medine Derin <Medine Derin@MEDINEDERIN44>
2023-11-25 17:34:14 +0300

    #Proje dosyası ekle.

--- Kitaplik/Controllers/BookController.cs
+++ Kitaplik/Controllers/BookController.cs
@@ -1,17 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Kitaplik.Models;
+using Kitaplik.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Kitaplik.Controllers;
 
 public class BookController : Controller
 {
+
+    // Dependency Injection
+
+    private readonly RepositoryBaglantisi _repository;
+
+    public BookController(RepositoryBaglantisi repository)
+    {
+        _repository = repository;
+    }
+
     public IActionResult Index()
     {
         return View();
     }
-
-    public IActionResult Create()
+    [HttpGet]
+        public IActionResult Create()
     {
         return View();
+    }
+
+    [HttpPost]  
+        public IActionResult Create(Book book)
+    {
+        _repository.Add(book);
+        _repository.SaveChanges();
+
+        return RedirectToAction("Index","Home");
     }
 
 }
